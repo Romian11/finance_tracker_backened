a65c5d94f02d26d0fa729322b2f9524e94ddb463
sahil verma <sahilvermajdp@gmail.com>
2024-07-17 17:53:53 +0530

    changes

--- config/connectDb.js
+++ config/connectDb.js
@@ -4,8 +4,9 @@ const connectDb = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL);
     console.log(`Server Running On ${mongoose.connection.host}`.bgCyan.white);
+    console.log("connection successfull");
   } catch (error) {
-    console.log(`${error}`.bgRed);
+    console.log(`error : ${error}`.bgRed);
   }
 };
 
